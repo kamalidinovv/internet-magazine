6815e015c90e05b9e2a9d68a49144c7bd3caa696
kamalidinovv <120557588+kamalidinovv@users.noreply.github.com>
2022-12-27 22:04:30 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {useThemeHook}from './GlobalComponents/ThemeProvider'
+import Header from "./commponents/Header";
+
+
+
+
+function App() {
+  const [theme]=useThemeHook()
+  return (
+    <main className={theme? 'bg-black': 'bg-light-2'}>
+      <Header/>
+
+    </main>
+  );
+}
+
+export default App;
